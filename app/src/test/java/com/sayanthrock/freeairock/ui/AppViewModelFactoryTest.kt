9490b2480b32bd63c76e14a228cbb6d2a487e078
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-06-25 00:28:11 +0530

    test: verify unified ViewModel factory

---
+++ app/src/test/java/com/sayanthrock/freeairock/ui/AppViewModelFactoryTest.kt
@@ -0,0 +1,42 @@
+package com.sayanthrock.freeairock.ui
+
+import com.sayanthrock.freeairock.data.github.GitHubApiService
+import com.sayanthrock.freeairock.data.storage.SecureStorageManager
+import io.mockk.mockk
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppViewModelFactoryTest {
+
+    private val secureStorage: SecureStorageManager = mockk(relaxed = true)
+    private val apiService: GitHubApiService = mockk(relaxed = true)
+    private val factory = AppViewModelFactory(secureStorage, apiService)
+
+    @Test
+    fun `creates AppViewModel`() {
+        val viewModel = factory.create(AppViewModel::class.java)
+
+        assertTrue(viewModel is AppViewModel)
+    }
+
+    @Test
+    fun `creates ReviewViewModel`() {
+        val viewModel = factory.create(ReviewViewModel::class.java)
+
+        assertTrue(viewModel is ReviewViewModel)
+    }
+
+    @Test
+    fun `creates ImageViewModel`() {
+        val viewModel = factory.create(ImageViewModel::class.java)
+
+        assertTrue(viewModel is ImageViewModel)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `throws for unknown ViewModel`() {
+        factory.create(UnknownViewModel::class.java)
+    }
+
+    private class UnknownViewModel : androidx.lifecycle.ViewModel()
+}
