c72d3815f1fd2bfaef21e48634852cf76183dda5
Sayanth Rock <202829406+SayanthRock@users.noreply.github.com>
2026-06-25 00:03:19 +0530

    feat: add navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
